41d1ed3af1316030f2c7cccba290e11e67064bb8
Victor Miller <vamiller12@gmail.com>
2020-08-05 15:29:48 -0400

    new db

--- ltl_project/settings.py
+++ ltl_project/settings.py
@@ -83,12 +83,13 @@ WSGI_APPLICATION = 'ltl_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
-
 
 
 # Password validation
